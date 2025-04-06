0ffc693dc572ba7466572e559468fb1cb247f056
shoeb <wiqbal091@gmail.com>
2025-04-06 18:09:35 +0530

    Bhosadike raaj aage ka kaam karo modern ui aur admin wagera ka

--- OnlineAssessment.MessageQueue/obj/Debug/net8.0/OnlineAssessment.MessageQueue.AssemblyInfo.cs
+++ OnlineAssessment.MessageQueue/obj/Debug/net8.0/OnlineAssessment.MessageQueue.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineAssessment.MessageQueue")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7954bacf8f6a151470a54152a60b175741e67668")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8572285e666dd59159275eaf941f464aa050a7c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineAssessment.MessageQueue")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineAssessment.MessageQueue")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
